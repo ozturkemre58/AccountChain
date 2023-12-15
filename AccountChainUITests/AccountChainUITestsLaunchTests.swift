8df334cdca19b2bb7c19aacf0084be176a7bc9c7
ozturkemre58 <emreozturk.5874@gmail.com>
2023-12-15 19:35:37 +0300

    Initial Commit

---
+++ AccountChainUITests/AccountChainUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AccountChainUITestsLaunchTests.swift
+//  AccountChainUITests
+//
+//  Created by Emre Öztürk on 15.12.2023.
+//
+
+import XCTest
+
+final class AccountChainUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
