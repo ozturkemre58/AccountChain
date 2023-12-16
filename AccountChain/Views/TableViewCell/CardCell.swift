c5a4a4dc5e3feb94bb0f8f062a09d28048890e3a
ozturkemre58 <emreozturk.5874@gmail.com>
2023-12-16 21:26:12 +0300

    Card cell icons added CardCell IBOutlets added

--- AccountChain/Views/TableViewCell/CardCell.swift
+++ AccountChain/Views/TableViewCell/CardCell.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class CardCell: UITableViewCell {
 
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var password: UIView!
+    
+    
+    @IBOutlet weak var editIcon: UIImageView!
+    @IBOutlet weak var removeIcon: UIImageView!
+    @IBOutlet weak var emailCopyIcon: UIView!
+    @IBOutlet weak var usernameCopyIcon: UIImageView!
+    @IBOutlet weak var passwordCopyIcon: UIImageView!
+    @IBOutlet weak var showPasswordIcon: UIImageView!
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    
+    
 }
