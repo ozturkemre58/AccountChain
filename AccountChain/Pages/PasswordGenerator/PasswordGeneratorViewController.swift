feb265849c3796dad4d15ff2c1cc41b807cea9c6
ozturkemre58 <emreozturk.5874@gmail.com>
2023-12-22 22:57:00 +0300

    Password generate func added password generare logics added

--- AccountChain/Pages/PasswordGenerator/PasswordGeneratorViewController.swift
+++ AccountChain/Pages/PasswordGenerator/PasswordGeneratorViewController.swift
@@ -10,20 +10,41 @@ import Firebase
 
 class PasswordGeneratorViewController: UIViewController {
 
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var copyButton: UIButton!
+    @IBOutlet weak var generateButton: UIButton!
     
-    
+    let viewModel: PasswordGeneratorViewModel = PasswordGeneratorViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
-
+        self.configView()
     }
+    
+    func configView() {
+        self.copyButton.isHidden = true
+        self.passwordLabel.text = ""
+        self.copyButton.layer.cornerRadius = 10
+        self.generateButton.layer.cornerRadius = 10
+        self.generateButton.layer.borderColor = UIColor(hex: "3066BE")?.cgColor
+        self.generateButton.layer.borderWidth = 1.0
+    }
+    
+    
+    @IBAction func generateAction(_ sender: Any) {
+        self.passwordLabel.text = self.viewModel.generatePassword()
+        self.copyButton.isHidden = false
+    }
+    
+    
+    @IBAction func copyAction(_ sender: Any) {
+        let pasteboard = UIPasteboard.general
 
-    @IBAction func signOut(_ sender: Any) {
-        do {
-           try  Auth.auth().signOut()
-        } catch {
-            print("Sign Out Error")
-        }
+        pasteboard.string = self.passwordLabel.text
+
+               let alert = UIAlertController(title: "\(pasteboard.string ?? "")", message: "Metin kopyalandı!", preferredStyle: .alert)
+               alert.addAction(UIAlertAction(title: "Tamam", style: .default, handler: nil))
+               present(alert, animated: true, completion: nil)
     }
 }
--- AccountChain/Pages/PasswordGenerator/ViewModel/PasswordGeneratorViewModel.swift
+++ AccountChain/Pages/PasswordGenerator/ViewModel/PasswordGeneratorViewModel.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
-class PasswordGeneratorViewModel {
+class PasswordGeneratorViewModel {
     
+    func generatePassword() -> String {
+        let allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:'<>,.?/"
+           var password = ""
+
+           for _ in 0..<13 {
+               let randomIndex = Int(arc4random_uniform(UInt32(allowedChars.count)))
+               let randomChar = allowedChars[allowedChars.index(allowedChars.startIndex, offsetBy: randomIndex)]
+               password.append(randomChar)
+           }
+
+           return password
+    }
 }
