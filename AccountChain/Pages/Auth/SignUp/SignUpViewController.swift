10776c82b3c550c3a6cbc83f774f74c728ce28ec
ozturkemre58 <emreozturk.5874@gmail.com>
2024-01-08 00:53:41 +0300

    SignUp Page ui added

--- AccountChain/Pages/Auth/SignUp/SignUpViewController.swift
+++ AccountChain/Pages/Auth/SignUp/SignUpViewController.swift
@@ -6,24 +6,156 @@
 //
 
 import UIKit
+import SnapKit
 
 class SignUpViewController: UIViewController {
-
+    
+    var emailField = DefaultTextField()
+    var usernameField = DefaultTextField()
+    var passwordField = DefaultTextField()
+    var phoneField = DefaultTextField()
+    var welcomeLabel = UILabel()
+    var infoLabel = UILabel()
+    var phoneNumberInfo = UILabel()
+    var emailInfo = UILabel()
+    var usernameInfo = UILabel()
+    var passwordInfo = UILabel()
+    
+    
+    let viewModel: SignUpViewModel = SignUpViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        prepareView()
+        configView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func prepareView() {
+        
+        view.backgroundColor = .base
+   
+        view.addSubview(welcomeLabel)
+        welcomeLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(25)
+            make.left.equalToSuperview().offset(15)
+        }
+        
+        view.addSubview(infoLabel)
+        infoLabel.snp.makeConstraints { make in
+            make.top.equalTo(welcomeLabel.snp.bottom).offset(10)
+            make.left.equalToSuperview().offset(15)
+        }
+        
+        view.addSubview(phoneNumberInfo)
+        phoneNumberInfo.snp.makeConstraints { make in
+            make.top.equalTo(infoLabel.snp.bottom).offset(20)
+            make.left.equalToSuperview().offset(20)
+        }
+        
+        view.addSubview(phoneField)
+        phoneField.snp.makeConstraints { make in
+            make.top.equalTo(phoneNumberInfo.snp.bottom).offset(7.5)
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+            make.height.equalToSuperview().multipliedBy(0.075)
+        }
+        
+        view.addSubview(emailInfo)
+        emailInfo.snp.makeConstraints { make in
+            make.top.equalTo(phoneField.snp.bottom).offset(20)
+            make.left.equalToSuperview().offset(20)
+        }
+        
+        view.addSubview(emailField)
+        emailField.snp.makeConstraints { make in
+            make.top.equalTo(emailInfo.snp.bottom).offset(7.5)
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+            make.height.equalToSuperview().multipliedBy(0.075)
+        }
+        
+        view.addSubview(usernameInfo)
+        usernameInfo.snp.makeConstraints { make in
+            make.top.equalTo(emailField.snp.bottom).offset(20)
+            make.left.equalToSuperview().offset(20)
+        }
+        
+        view.addSubview(usernameField)
+        usernameField.snp.makeConstraints { make in
+            make.top.equalTo(usernameInfo.snp.bottom).offset(7.5)
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+            make.height.equalToSuperview().multipliedBy(0.075)
+        }
+        
+        view.addSubview(passwordInfo)
+        passwordInfo.snp.makeConstraints { make in
+            make.top.equalTo(usernameField.snp.bottom).offset(20)
+            make.left.equalToSuperview().offset(20)
+        }
+        
+        view.addSubview(passwordField)
+        passwordField.snp.makeConstraints { make in
+            make.top.equalTo(passwordInfo.snp.bottom).offset(7.5)
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+            make.height.equalToSuperview().multipliedBy(0.075)
+        }
     }
-    */
-
+    
+    func configView() {
+                
+        //welcomeLabel
+        welcomeLabel.text = "Welcome"
+        welcomeLabel.font = .boldSystemFont(ofSize: 26)
+        welcomeLabel.textColor = .whiteBlack
+        
+        //infoLabel
+        infoLabel.text = "Enter Your Details To Sign Up"
+        infoLabel.textColor = .whiteBlack
+        
+        //phoneInfoLabel
+        phoneNumberInfo.text = "Phone Number"
+        phoneNumberInfo.textColor = .whiteBlack
+        //phoneField
+        phoneField.backgroundColor = .whiteBlack
+        phoneField.placeholder = "Enter Your Phone Number"
+        phoneField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 40))
+        phoneField.leftViewMode = .always
+        phoneField.layer.cornerRadius = 5
+        phoneField.keyboardType = .phonePad
+        
+        //emailFieldInfo
+        emailInfo.text = "Email"
+        emailInfo.textColor = .whiteBlack
+        //emailField
+        emailField.backgroundColor = .whiteBlack
+        emailField.placeholder = "Enter Your Email"
+        emailField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 40))
+        emailField.leftViewMode = .always
+        emailField.layer.cornerRadius = 5
+        emailField.keyboardType = .emailAddress
+        
+        //usernameInfo
+        usernameInfo.text = "Username"
+        usernameInfo.textColor = .whiteBlack
+        //usernameField
+        usernameField.backgroundColor = .whiteBlack
+        usernameField.placeholder = "Enter Your Username"
+        usernameField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 40))
+        usernameField.leftViewMode = .always
+        usernameField.layer.cornerRadius = 5
+        
+        //passwordInfo
+        passwordInfo.text = "Password"
+        passwordInfo.textColor = .whiteBlack
+        //passwordField
+        passwordField.backgroundColor = .whiteBlack
+        passwordField.placeholder = "Enter Your Password"
+        passwordField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 40))
+        passwordField.leftViewMode = .always
+        passwordField.layer.cornerRadius = 5
+    }
+    
 }
