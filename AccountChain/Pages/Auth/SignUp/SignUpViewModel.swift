62295ed4f8920a66a643d15d28bc95867065fc40
ozturkemre58 <emreozturk.5874@gmail.com>
2024-01-08 00:53:19 +0300

    Sign Up ViewModel added

--- AccountChain/Pages/Auth/SignUp/SignUpViewModel.swift
+++ AccountChain/Pages/Auth/SignUp/SignUpViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import Firebase
+
+class SignUpViewModel {
+    
+    func signUpAction(email: String?, password: String?, completion: @escaping (Bool) -> Void) {
+        Auth.auth().createUser(withEmail: email ?? "", password: password ?? "") { (result, error) in
+            if error != nil {
+                print("Sign Up Error: \(String(describing: error?.localizedDescription))")
+                MessageManager.shared.show(message: error?.localizedDescription ?? "", type: .error)
+                completion(false)
+            } else {
+                ConstantManager.shared.dbKey = result?.user.uid ?? ""
+                completion(true)
+            }
+        }
+    }
+}
